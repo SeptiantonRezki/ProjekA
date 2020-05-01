d0fedc16e6f548d80f41c22be7f678deaa56c45d
SeptiantonRezki <Septianrezki398@gmail.com>
2020-05-01 22:45:10 +0700

    sudah menambahkan style login dan register

--- src/containers/pages/Home/index.js
+++ src/containers/pages/Home/index.js
@@ -3,9 +3,11 @@ import {View, Text} from 'react-native';
 
 const Home = () => {
     return (
-        <View style={{paddingHorizontal : 20 , paddingTop : 10, flex : 1}}>
-            <View>
-
+        <View style={{paddingHorizontal : 20 , paddingTop : 10, flex : 1, backgroundColor : 'green'}}>
+            <View style={{backgroundColor : white, opacity : 0.5, height : 10}}>
+                <Text>
+                    
+                </Text>
             </View>
             <View></View>
             <View></View>
